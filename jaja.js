5efa7081ca1064624cb2526a33b990cc99566893
Khaled Ali Ahmed <40940497+Blackbirdecho@users.noreply.github.com>
2019-01-25 00:14:10 +0530

    Add files via upload

---
+++ jaja.js
@@ -0,0 +1,11 @@
+(function($){
+    'use strict';
+      $(window).on('load', function () {
+          if ($(".pre-loader").length > 0)
+          {
+              $(".pre-loader").fadeOut("slow");
+          }
+      });
+  })(jQuery)
+
+  
